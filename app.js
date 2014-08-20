224cc2f731570695ebcca3683450880726cc65bb
delwiv <delwiv@gmail.com>
2014-08-20 16:59:03 +0200

    working quite well ;)

--- app.js
+++ app.js
@@ -3,41 +3,69 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var fs = require('fs');
+
+var users = [];
+
+eval(fs.readFileSync('./public/javascript/User.js').toString());
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res){
-  res.sendFile('/home/delwiv/dev/git/tuto_node_chat/index.html');
+	res.sendFile('/home/delwiv/dev/git/tuto_node_chat/index.html');
   // console.log('1 request handled ;)');
 });
 
 io.on('connection', function(socket){
-    var connectedUsers = [];
-    console.log('a user connected');
-    socket.on('disconnect', function(username){
-        
+    // console.log('a user connected');
+
+    socket.on('addUser', function(user){
+    	// socket.user = userName;
+    	console.log(user.name + ' has joined.');
+    	users.push(user);
+    	console.log(users);
+    	io.emit('chat message', 'Info : ' + user.name + ' joined the chat.');
+    	updateClients();
     });
-    socket.on('userLeft', function(username){
-    	console.log('user ' + username + ' disconnected');
-    	io.emit('user left', userName);
+    
+    
+    socket.on('state changed', function(user){
+    	console.log('state changed: ' + user.name + ' : ' + user.status);
+    	users[getIndex(users, user.name)].status = user.status;
+    	debugger;
+    	updateClients();    	
     });
-    socket.on('state changed', function(msg){
-        console.log('state changed: ' + msg);
+    
+    socket.on('chat message', function(userName, msg){
+    	console.log('message from ' + userName + ' : ' + msg);
+    	users[getIndex(users, userName)].status = "inactive";
+    	io.emit('chat message', userName + ' : ' + msg);
+    	updateClients();
     });
-    socket.on('chat message', function(msg){
-        console.log('message : ' + msg);
-        io.emit('chat message', msg);
-    });
-    socket.on('user identified', function(userName){
-        connectedUsers[connectedUsers.length] = userName;
-        io.emit('new user', userName);
-    });
+    
+    updateClients = function(){
+    	debugger;
+    	io.emit('update', users);
+    }
 
 });
 
+io.on('disconnect', function(){
+	debugger;
+	for( var i = 0; i < users.length; i++ ){
+		if(users[i] == socket.user){
+			console.log('a user diconnected');
+			io.emit('chat message', 'Info : ' + users[i] + ' has left the chat.');
+			delete users[users[i]];
+		}
+	}
+	updateClients();
+});
+
+
 
 http.listen(3000, function(){
-  console.log('listening on localhost:3000');
+	console.log('listening on localhost:3000');
 });
 
 // module.exports = app;
---
+++ public/javascript/User.js
@@ -0,0 +1,13 @@
+function User(name){
+	this.name = name;
+	this.status = "inactive";
+}
+
+function getIndex(arr,name) {
+	for (var i = 0 ; i < arr.length ; i++ ){
+		if ( arr[i].name === name) {
+			return i;
+		}
+	}
+    return -1;
+}
